74d1a77369fa24b31ad801088d6e1186730edadc
oldlaptop <oldlaptop654@aol.com>
2013-11-11 18:58:53 -0500

    Crazy Spathi
     This uses a different implementation for the shotgun than
     Crazy Mod 1.4.3 did. The original used a 'two-pass' method,
     creating half the projectiles in initialize_standard_missile
     and half in a special function which was called from
     spathi_postprocess. EP says this was to work around a bug
     with creating many elements at once - it *seems* to work
     fine now, so we simply create all of them in the normal
     init_weapon_func.

--- sc2/src/uqm/ships/spathi/spathi.c
+++ sc2/src/uqm/ships/spathi/spathi.c
@@ -19,6 +19,8 @@
 #include "../ship.h"
 #include "spathi.h"
 #include "resinst.h"
+#include "uqm/globdata.h"
+#include "uqm/colors.h"
 
 // Core characteristics
 #define MAX_CREW 30
@@ -32,25 +34,26 @@
 #define SHIP_MASS 5
 
 // Forward gun
-#define WEAPON_ENERGY_COST 2
+#define WEAPON_ENERGY_COST MAX_ENERGY
 #define WEAPON_WAIT 0
 #define SPATHI_FORWARD_OFFSET 16
 #define MISSILE_SPEED DISPLAY_TO_WORLD (30)
 #define MISSILE_LIFE 10
-#define MISSILE_HITS 1
-#define MISSILE_DAMAGE 1
+#define MISSILE_HITS 3
+#define MISSILE_DAMAGE 2
 #define MISSILE_OFFSET 1
+#define NUM_MISSILES 36
 #define MISSILE_RANGE (MISSILE_SPEED * MISSILE_LIFE)
 		/* This is for the cyborg only. */
 
 // B.U.T.T.
-#define SPECIAL_ENERGY_COST 3
-#define SPECIAL_WAIT 7
+#define SPECIAL_ENERGY_COST 2
+#define SPECIAL_WAIT 18
 #define SPATHI_REAR_OFFSET 20
-#define DISCRIMINATOR_SPEED DISPLAY_TO_WORLD (8)
+#define DISCRIMINATOR_SPEED DISPLAY_TO_WORLD (16)
 #define DISCRIMINATOR_LIFE 30
 #define DISCRIMINATOR_HITS 1
-#define DISCRIMINATOR_DAMAGE 2
+#define DISCRIMINATOR_DAMAGE 3
 #define DISCRIMINATOR_OFFSET 4
 #define TRACK_WAIT 1
 
@@ -59,7 +62,7 @@ static RACE_DESC spathi_desc =
 	{ /* SHIP_INFO */
 		"eluder",
 		FIRES_FORE | FIRES_AFT | SEEKING_SPECIAL | DONT_CHASE,
-		18, /* Super Melee cost */
+		35, /* Super Melee cost */
 		MAX_CREW, MAX_CREW,
 		MAX_ENERGY, MAX_ENERGY,
 		SPATHI_RACE_STRINGS,
@@ -133,10 +136,62 @@ butt_missile_preprocess (ELEMENT *ElementPtr)
 		--ElementPtr->turn_wait;
 	else
 	{
-		COUNT facing;
+		ELEMENT *EnemyElementPtr;
+		COUNT facing, delta_facing;
+		/* SWORD not COUNT, since GetNextVelocityComponents wants a
+		 * signed var */
+		SWORD delta_x, delta_y;
+		COUNT num_frames;
 
 		facing = GetFrameIndex (ElementPtr->next.image.frame);
-		if (TrackShip (ElementPtr, &facing) > 0)
+
+		if(ElementPtr->hTarget)
+		{
+			LockElement (ElementPtr->hTarget, &EnemyElementPtr);
+			delta_x = EnemyElementPtr->current.location.x
+					- ElementPtr->current.location.x;
+			delta_x = WRAP_DELTA_X (delta_x);
+			delta_y = EnemyElementPtr->current.location.y
+					- ElementPtr->current.location.y;
+			delta_y = WRAP_DELTA_Y (delta_y);
+	
+			num_frames = (square_root ((long)delta_x * delta_x
+					+ (long)delta_y * delta_y)) / DISCRIMINATOR_SPEED;
+			if (num_frames == 0)
+				num_frames = 1;
+	
+			GetNextVelocityComponents (&EnemyElementPtr->velocity,
+					&delta_x, &delta_y, num_frames);
+	
+			delta_x = (EnemyElementPtr->current.location.x + (delta_x / 2))
+					- ElementPtr->current.location.x;
+			delta_y = (EnemyElementPtr->current.location.y + (delta_y / 2))
+					- ElementPtr->current.location.y;
+	
+			delta_facing = NORMALIZE_FACING (
+					ANGLE_TO_FACING (ARCTAN (delta_x, delta_y)) - facing);
+	
+			if (delta_facing > 0 && !OBJECT_CLOAKED(EnemyElementPtr))
+			{
+				if (delta_facing == ANGLE_TO_FACING (HALF_CIRCLE))
+					facing += (((BYTE)TFB_Random () & 1) << 1) - 1;
+				else if (delta_facing < ANGLE_TO_FACING (HALF_CIRCLE))
+					++facing;
+				else
+					--facing;
+			}
+	
+			ElementPtr->next.image.frame =
+					SetAbsFrameIndex (ElementPtr->next.image.frame,
+					facing);
+			ElementPtr->state_flags |= CHANGING;
+	
+			SetVelocityVector (&ElementPtr->velocity,
+					DISCRIMINATOR_SPEED, facing);
+
+			UnlockElement (ElementPtr->hTarget);
+		}
+		else if (TrackShip (ElementPtr, &facing) > 0)
 		{
 			ElementPtr->next.image.frame =
 					SetAbsFrameIndex (ElementPtr->next.image.frame,
@@ -245,29 +300,81 @@ spathi_intelligence (ELEMENT *ShipPtr, EVALUATE_DESC *ObjectsOfConcern,
 	}
 }
 
-static COUNT
-initialize_standard_missile (ELEMENT *ShipPtr, HELEMENT MissileArray[])
+MISSILE_BLOCK setup_for_one_primary_shot(ELEMENT *ShipPtr)
 {
+#define SPATHI_FORWARD_OFFSET 16
+#define MISSILE_HITS 3 //1
+#define MISSILE_DAMAGE 2 //1
+#define MISSILE_OFFSET 1
+#define NUM_MISSILES 36
 	STARSHIP *StarShipPtr;
 	MISSILE_BLOCK MissileBlock;
 
 	GetElementStarShip (ShipPtr, &StarShipPtr);
-	MissileBlock.cx = ShipPtr->next.location.x;
-	MissileBlock.cy = ShipPtr->next.location.y;
 	MissileBlock.farray = StarShipPtr->RaceDescPtr->ship_data.weapon;
-	MissileBlock.face = MissileBlock.index = StarShipPtr->ShipFacing;
 	MissileBlock.sender = ShipPtr->playerNr;
 	MissileBlock.flags = IGNORE_SIMILAR;
 	MissileBlock.pixoffs = SPATHI_FORWARD_OFFSET;
-	MissileBlock.speed = MISSILE_SPEED;
 	MissileBlock.hit_points = MISSILE_HITS;
 	MissileBlock.damage = MISSILE_DAMAGE;
-	MissileBlock.life = MISSILE_LIFE;
 	MissileBlock.preprocess_func = NULL;
 	MissileBlock.blast_offs = MISSILE_OFFSET;
-	MissileArray[0] = initialize_missile (&MissileBlock);
 
-	return (1);
+	MissileBlock.cx = ShipPtr->next.location.x + COSINE (FACING_TO_ANGLE (StarShipPtr->ShipFacing + 4), (COUNT)TFB_Random() % 261 - 130);
+	MissileBlock.cy = ShipPtr->next.location.y + SINE (FACING_TO_ANGLE (StarShipPtr->ShipFacing + 4), (COUNT)TFB_Random() % 261 - 130);
+
+	MissileBlock.face = MissileBlock.index = StarShipPtr->ShipFacing;
+
+	MissileBlock.speed = MISSILE_SPEED + ((COUNT)TFB_Random() % ((DISPLAY_TO_WORLD(20) + 1))) - DISPLAY_TO_WORLD(10);
+	MissileBlock.life = MISSILE_LIFE + TFB_Random() % 17 - 8;
+
+	return MissileBlock;
+}
+
+static void
+fix_one_primary_shot(ELEMENT *MissilePtr)
+{
+	SIZE angle, dx, dy, speed;
+
+	angle = GetVelocityTravelAngle (&MissilePtr->velocity);
+	GetCurrentVelocityComponents(&MissilePtr->velocity, &dx, &dy);
+
+	speed = square_root (dx*dx + dy*dy);
+
+	angle += ((COUNT)TFB_Random() % 9) - 4;
+
+	SetVelocityComponents(&MissilePtr->velocity, COSINE(angle, speed), SINE(angle, speed));
+}
+
+static COUNT
+initialize_standard_missile (ELEMENT *ShipPtr, HELEMENT MissileArray[])
+{
+	COUNT i;
+	MISSILE_BLOCK MissileBlock;
+
+	for (i = 0; i < NUM_MISSILES; i++)
+	{
+		MissileBlock = setup_for_one_primary_shot(ShipPtr);
+
+		MissileArray[i] = initialize_missile (&MissileBlock);
+		
+		if (!MissileArray[i])
+		{
+			return i;
+		}
+		else
+		{
+			ELEMENT* MissilePtr;
+
+			LockElement (MissileArray[i], &MissilePtr);
+
+			fix_one_primary_shot(MissilePtr);
+
+			UnlockElement (MissileArray[i]);
+		}
+	}
+
+	return (NUM_MISSILES);
 }
 
 static void
@@ -278,7 +385,7 @@ spathi_postprocess (ELEMENT *ElementPtr)
 	GetElementStarShip (ElementPtr, &StarShipPtr);
 	if ((StarShipPtr->cur_status_flags & SPECIAL)
 			&& StarShipPtr->special_counter == 0
-			&& DeltaEnergy (ElementPtr, -SPECIAL_ENERGY_COST))
+			&& CleanDeltaEnergy (ElementPtr, -SPECIAL_ENERGY_COST))
 	{
 		spawn_butt_missile (ElementPtr);
 
